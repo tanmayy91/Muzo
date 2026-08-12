ebcc66acb6ca06a6dc6933d27443472472029c90
Tanmay S. <nitrotanmay538@gmail.com>
2026-08-12 16:07:54 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,16 +15,25 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
 
+// Fix JVM target mismatch across all subproject plugins (including dynamic_color)
 subprojects {
+    plugins.withId("com.android.library") {
+        configure<com.android.build.gradle.LibraryExtension> {
+            compileOptions {
+                sourceCompatibility = JavaVersion.VERSION_17
+                targetCompatibility = JavaVersion.VERSION_17
+            }
+        }
+    }
+
     tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
         kotlinOptions {
-            if (project.name != "app") {
-                jvmTarget = "17"
-            }
+            jvmTarget = "17"
         }
     }
 }
